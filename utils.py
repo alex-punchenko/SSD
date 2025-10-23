fc5be925100b0d1e43fa7b1f15eccc2f1bd7050e
Alex Punchenko <63407441+alex-punchenko@users.noreply.github.com>
2025-10-23 12:00:09 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,16 +8,19 @@ import torchvision.transforms.functional as FT
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Label map
+bccd_labels = ('RBC', 'WBC', 'Platelets')
+label_map = {k: v + 1 for v, k in enumerate(bccd_labels)}
+label_map['background'] = 0
+
+rev_label_map = {v: k for k, v in label_map.items()}
+
 voc_labels = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
               'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
 label_map = {k: v + 1 for v, k in enumerate(voc_labels)}
 label_map['background'] = 0
 rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
 
-# Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
-distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
-                   '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000',
-                   '#ffd8b1', '#e6beff', '#808080', '#FFFFFF']
+distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8']  # 3 класса + background
 label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
 
 
